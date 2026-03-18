c0e9d04e0a350f7bb0150a6c157e2dff34f869ee
Raghava Rahul Kalavala <raghavarahul_kalavala@epam.com>
2026-03-18 18:17:07 +0530

    Adding User Agent Schema

--- src/agents/schema.py
+++ src/agents/schema.py
@@ -1,7 +1,16 @@
 from pydantic import BaseModel, Field
-
+ 
 class UserSearchBookModel(BaseModel):
-    query: str = Field(min_length=3, max_length=500, 
+    query: str = Field(min_length=3, max_length=500,
                        description="If you want to search for a book, you can provide the book title or author. "
                                    "Agent will also provide search results for the book title or author along with a brief description.")
-    
+ 
+class AgentBookResultModel(BaseModel):
+    title: str
+    description: str
+    agent_description: str
+ 
+class AgentBookSearchResponseModel(BaseModel):
+    results: list[AgentBookResultModel]
+ 
+ 
